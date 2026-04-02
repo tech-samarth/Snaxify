23a07e835c7e3b4cf723a828f3d2186a76b7722d
Samarth Sehrawat <164867341+tech-samarth@users.noreply.github.com>
2026-04-03 01:34:21 +0530

    Create products.js

---
+++ products.js
@@ -0,0 +1,16 @@
+[
+  {"id":5,"name":"Coffee","image":"https://raw.githubusercontent.com/techsamarth/public/refs/heads/main/coffee.jpg","price":15},
+  {"id":6,"name":"Raw Maggie","image":"https://raw.githubusercontent.com/techsamarth/public/refs/heads/main/Maggie.jpg","price":18},
+  {"id":7,"name":"Kurkure","image":"https://raw.githubusercontent.com/techsamarth/public/refs/heads/main/kurkure.jpg","price":25},
+  {"id":8,"name":"Red Lays","image":"https://raw.githubusercontent.com/techsamarth/public/refs/heads/main/lays.jpg","price":25},
+  {"id":10,"name":"Green Lays","image":"https://raw.githubusercontent.com/tech-samarth/public/refs/heads/main/blue%20lays.jpeg","price":25},
+  {"id":11,"name":"Blue Lays","image":"https://raw.githubusercontent.com/tech-samarth/public/refs/heads/main/greenlays.jpeg","price":25},
+  {"id":12,"name":"Puffcorn","image":"https://raw.githubusercontent.com/tech-samarth/public/refs/heads/main/puffcorn.jpeg","price":25},
+  {"id":15,"name":"Dark Fantacy OREO","image":"https://raw.githubusercontent.com/tech-samarth/public/refs/heads/main/oreo.jpeg","price":30},
+  {"id":16,"name":"Dark Fantacy Bourboun","image":"https://raw.githubusercontent.com/tech-samarth/public/refs/heads/main/bourboun.jpeg","price":30},
+  {"id":17,"name":"Smooth Caramel","image":"https://raw.githubusercontent.com/tech-samarth/public/refs/heads/main/smooth.jpg","price":15},
+  {"id":18,"name":"Manchow Veg Soup","image":"https://raw.githubusercontent.com/tech-samarth/public/refs/heads/main/manchow.png","price":20},
+  {"id":19,"name":"Chatpata Tomato Soup","image":"https://raw.githubusercontent.com/tech-samarth/public/refs/heads/main/tomato.png","price":20},
+  {"id":20,"name":"Hot & Sour Veg Soup","image":"https://raw.githubusercontent.com/tech-samarth/public/refs/heads/main/veg.png","price":20}
+
+]
